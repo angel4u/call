acd286fcb2fcea4843ff4b85a6e373b14b545312
root <root@ip-172-31-26-145.us-east-2.compute.internal>
2021-02-22 14:10:28 +0000

    initial commit

---
+++ build/precache-manifest.a83d60259af601e57d7b876e1b985bb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aeec65928a25590578c2dde918ac13e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b43968db61f0b00a3cc",
+    "url": "/static/css/main.6fd2da93.chunk.css"
+  },
+  {
+    "revision": "2c61047efc24988d18e4",
+    "url": "/static/js/2.7b63cf42.chunk.js"
+  },
+  {
+    "revision": "861aebbddd875d5250794c1881af1f83",
+    "url": "/static/js/2.7b63cf42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b43968db61f0b00a3cc",
+    "url": "/static/js/main.9038d151.chunk.js"
+  },
+  {
+    "revision": "40cb58517e0c2c42111a",
+    "url": "/static/js/runtime-main.a44a3e6c.js"
+  }
+]);
